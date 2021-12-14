0b6baeb87c09bb765a6dcf11b3f950da3bf55168
Alex <mav735@ya.ru>
2021-12-14 21:17:08 +0300

    Player merged with camera

--- player.py
+++ player.py
@@ -8,22 +8,33 @@ class Player:
         self.armor = 0
         self.inventory = []
         self.current_object = None
-
+        self.x = 10
+        self.y = 10
         self.move_anime = False
         self.attack_anime = False
         self.damage_anime = False
         self.dying_anime = False
 
-    def movement(self, event, cam):
-        if event.type == pygame.K_w:
-            cam.x_move(1)
-            self.move_anime = True
-        elif event.type == pygame.K_s:
-            cam.x_move(-1)
-            self.move_anime = True
-        elif event.type == pygame.K_a:
-            cam.y_move(-1)
-            self.move_anime = True
-        elif event.type == pygame.K_d:
-            cam.y_move(1)
-            self.move_anime = True
+    def get_coords(self):
+        return self.x, self.y
+
+    def x_move(self, coefficient):
+        self.x += 3 * coefficient
+
+    def y_move(self, coefficient):
+        self.y += 3 * coefficient
+
+    def movement(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                self.y_move(1)
+                self.move_anime = True
+            if event.key == pygame.K_s:
+                self.y_move(-1)
+                self.move_anime = True
+            if event.key == pygame.K_a:
+                self.x_move(1)
+                self.move_anime = True
+            if event.key == pygame.K_d:
+                self.x_move(-1)
+                self.move_anime = True
