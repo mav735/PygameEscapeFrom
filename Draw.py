84f4b126e772b106b2762deff5b9e55e5b03c11e
Cyber-Zhaba <20096ninja@gmail.com>
2021-12-20 23:17:07 +0300

    added new void texture

--- Draw.py
+++ Draw.py
@@ -10,21 +10,32 @@ class DrawFloor:
         with open("Settings.cfg", "r") as SettingsFile:
             self.cell_size = int(SettingsFile.readlines()[-1].rstrip())
         self.type_texture = type_texture
-        self.materials = {'1': pygame.transform.scale(pygame.image.load(os.path.join('Floor', 'floor1.png')),
-                                                      (self.cell_size, self.cell_size)),
-                          '2': pygame.transform.scale(pygame.image.load(os.path.join('Walls', 'wall2.png')),
-                                                      (self.cell_size, self.cell_size)),
-                          '3': pygame.transform.scale(pygame.image.load(os.path.join('Walls', 'wall3.png')),
-                                                      (self.cell_size, self.cell_size)),
-                          '4': pygame.transform.scale(pygame.image.load(os.path.join('Walls', 'wall4.png')),
-                                                      (self.cell_size, self.cell_size)),
-                          '5': pygame.transform.scale(pygame.image.load(os.path.join('Walls', 'wall5.png')),
-                                                      (self.cell_size, self.cell_size)),
-                          '6': pygame.transform.scale(pygame.image.load(os.path.join('Walls', 'wall6.png')),
-                                                      (self.cell_size, self.cell_size)),
-                          '7': pygame.transform.scale(pygame.image.load(os.path.join('Walls', 'wall7.png')),
-                                                      (self.cell_size, self.cell_size))
-                          }
+        self.materials = {
+            '0': pygame.transform.scale(
+                pygame.image.load(os.path.join('Floor', 'void.png')),
+                (self.cell_size, self.cell_size)),
+            '1': pygame.transform.scale(
+                pygame.image.load(os.path.join('Floor', 'floor1.png')),
+                (self.cell_size, self.cell_size)),
+            '2': pygame.transform.scale(
+                pygame.image.load(os.path.join('Walls', 'wall2.png')),
+                (self.cell_size, self.cell_size)),
+            '3': pygame.transform.scale(
+                pygame.image.load(os.path.join('Walls', 'wall3.png')),
+                (self.cell_size, self.cell_size)),
+            '4': pygame.transform.scale(
+                pygame.image.load(os.path.join('Walls', 'wall4.png')),
+                (self.cell_size, self.cell_size)),
+            '5': pygame.transform.scale(
+                pygame.image.load(os.path.join('Walls', 'wall5.png')),
+                (self.cell_size, self.cell_size)),
+            '6': pygame.transform.scale(
+                pygame.image.load(os.path.join('Walls', 'wall6.png')),
+                (self.cell_size, self.cell_size)),
+            '7': pygame.transform.scale(
+                pygame.image.load(os.path.join('Walls', 'wall7.png')),
+                (self.cell_size, self.cell_size))
+        }
         self.map = Map
 
     def blit_floor(self, coords):
